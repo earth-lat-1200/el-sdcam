bf44dca982f7de826c6d66aa60d9c2a14cc935f6
SFillip <sfillip2005@gmail.com>
2023-06-19 08:23:57 +0200

    Log format changed Commit

--- source/script/sdUpdate.py
+++ source/script/sdUpdate.py
@@ -6,7 +6,7 @@ gitUrl = git.cmd.Git("https://github.com/SFillip/el-sdcam.git")
 logging.basicConfig(filename='updateLog.log', encoding='utf-8', level=logging.INFO)
 try : 
     ##Wenn pull erfolgreich war wird Status und Zeit geloggt
-    logging.info(gitUrl.pull() + ' ' + str(datetime.datetime.now()))
+    logging.info( str(datetime.datetime.now()  + '\n' +gitUrl.pull()))
 except Exception as e :
     ##Wenn pull nicht erfolgreich war wird entsprechende Fehlermeldung und Zeit geloggt
-    logging.warning(str(e) + ' ' + str(datetime.datetime.now()))
+    logging.warning(str(datetime.datetime.now() + '\n' + str(e)))
